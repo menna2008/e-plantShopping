a080179545eb9e954691c8dad17651a917f583e3
menna2008 <menna.halawa08@gmail.com>
2025-06-26 16:29:08 +0400

    fixed issues pt. 2

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,7 +9,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
-    return cart.reduce((total, item) => total + parseFloat(item.cost.substring(1)) * item.quantity, 0)
+    return cart.length ? cart.reduce((total, item) => total + parseFloat(item.cost.substring(1)) * item.quantity, 0) : 0
   };
 
   const handleContinueShopping = (e) => {
@@ -22,7 +22,9 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleDecrement = (item) => {
-    dispatch(updateQuantity({name : item.name, quantity : item.quantity + 1}))
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({name : item.name, quantity : item.quantity + 1}))
+    }
   };
 
   const handleRemove = (item) => {
